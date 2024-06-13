7a6a90f9140d7538d4d111f76053f39877e6c926
Vanshika1704 <vanshikawadhwa1704@gmail.com>
2024-06-13 12:54:24 +0530

    broke down a different component Cards- used props to pass data btw App.vue and Cards.vue

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,17 @@
 <script setup>
 import q from "./data/quizes.json"
 import {ref, watch} from "vue"
+import Card from "./components/Card.vue"
 
 const quizes = ref(q)
 const search=ref("")
 
-watch(search,()=>{  //to update quizes list based on search text
+watch(search,()=>{  //to update quizes list based on search text->watch the changes constantly and update constantly
 quizes.value=q.filter(quiz=>quiz.name.toLowerCase().includes(search.value.toLowerCase()))
 })
+
 </script>
+
 <template>
   <div class="container">
     <header>
@@ -18,16 +21,18 @@ quizes.value=q.filter(quiz=>quiz.name.toLowerCase().includes(search.value.toLowe
       <input type="text" v-model.trim="search" placeholder="Search...">
     </header>
     <div class="options-conatiner">
-      <div v-for="quiz in quizes" :key="quiz.id" class="card">
+      <Card v-for="quiz in quizes" :key="quiz.id" :quiz="quiz"/>
+      <!-- <div v-for="quiz in quizes" :key="quiz.id" class="card">
         <img :src="quiz.img" alt="quizalt">
         <div class="card-text">
           <h2>{{ quiz.name }}</h2>
           <p>{{ quiz.questions.length }} Questions</p>
         </div>
-      </div>
+      </div> -->
     </div>
   </div>
 </template>
+
 <style scoped>
 .container{
 max-width: 1000px;
@@ -53,26 +58,5 @@ header input{
   display: flex;
   flex-wrap: wrap;
   margin-top: 40px;
-}
-/* card */
-.card {
-  width: 310px;
-  overflow: hidden;
-  border-radius: 2%;
-  box-shadow: 1px 1px 10px rgba(0,0,0,0.1);
-  margin-bottom: 35px;
-  margin-right: 20px;
-  cursor: pointer;
-}
-.card img{
-  width: 100%;
-  height: 190px;
-  margin: 0;
-}
-.card .card-text{
-  padding: 0 5px;
-}
-.card .card-text h2{ 
-  font-weight: bold;
 }
 </style>
