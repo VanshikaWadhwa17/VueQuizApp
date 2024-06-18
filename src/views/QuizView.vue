f61c74a2ebc6a5f53f5b1e522c45e83daee2e6ea
Vanshika1704 <vanshikawadhwa1704@gmail.com>
2024-06-18 21:08:23 +0530

    Making questions and options -> then changing them from hard coded to dynamic using a jsonfile containing data

--- src/views/QuizView.vue
+++ src/views/QuizView.vue
@@ -1,5 +1,23 @@
+<script setup>
+import QuizHeader from "../components/QuizHeader.vue"
+import Question from "../components/Question.vue"
+import {useRoute} from "vue-router"
+import { ref } from "vue";
+import quizes from "../data/quizes.json"
+
+const route=useRoute()
+
+const quizId=parseInt(route.params.id);
+const quiz=quizes.find(q=>q.id===quizId)
+const currentQuesIndex=ref(0)
+</script>
 <template>
     <div>
-        <h1>Quiz here</h1>
+       
+        <QuizHeader/>
+            <div>
+                <Question :question="quiz.questions[currentQuesIndex]"/>
+            </div>
+       
     </div>
 </template>
